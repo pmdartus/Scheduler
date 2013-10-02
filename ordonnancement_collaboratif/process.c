bdc380a5220ebfb6e06d65fa22e45c2bacf66470
Dartus Pierre-Marie <pmdartus@if501-219-14.insa-lyon.fr>
2013-10-02 11:22:46 +0200

    q3.2 - init_ctx defined

--- ordonnancement_collaboratif/process.c
+++ ordonnancement_collaboratif/process.c
@@ -1,3 +1,8 @@
 void init_ctx(struct ctx_s* ctx, func_t f, unsigned int stack_size) {
+	//Initialisation of the context	
+	ctx.adr_instruction = (uint32_t) f;
+	ctx.adr_stack = (uint32_t) stack_size;
 	
+	//Stack allocation for pong (ping will use notmain stack :)
+	AllocateMemory(stack_size);
 }
--- ordonnancement_collaboratif/process.h
+++ ordonnancement_collaboratif/process.h
@@ -1,9 +1,9 @@
 
 struct ctx_s {
-	uint32_t* adr_inst;
-	uint32_t* adr_stack;
+	uint32_t adr_instruction;
+	uint32_t adr_stack;
 };
 
-ctx_s current_ctx;
+struct ctx_s* current_ctx;
 
 void init_ctx(struct ctx_s* ctx, func_t f, unsigned int stack_size);
