8247cd8edb7e7f877810f19738aa7b1c38e7a2e4
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2013-10-09 08:59:03 +0200

    It compiles

--- ordonnancement_collaboratif/dispatcher.c
+++ ordonnancement_collaboratif/dispatcher.c
@@ -2,13 +2,13 @@
 
 void switch_to(ctx_s* ctx) {
 	//Save current context
-	__asm("mov %14, sp" : "=r"(current_ctx->adr_instruction)); //LR because we want the following instruction
-	__asm("mov %0, sp" : "=r"((*current_ctx).adr_stack)); //stack pointer
+	__asm("mov %0, lr" : "=r"(current_ctx->adr_instruction)); //LR because we want the following instruction
+	__asm("mov %0, sp" : "=r"(current_ctx->adr_stack)); //stack pointer
 
 	//Switch
 	current_ctx = ctx;
 
 	//update the mc: assembleur to restaure new context
-	__asm("mov sp, %14" : : "r"((*current_ctx).adr_instruction));
-	__asm("mov sp, %0" : : "r"((*current_ctx).adr_stack));
+	__asm("mov lr, %0" : : "r"(current_ctx->adr_instruction));
+	__asm("mov sp, %0" : : "r"(current_ctx->adr_stack));
 }
