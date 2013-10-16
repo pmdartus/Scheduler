16281f4bbae578d2bfcd53b2d29264466c105c3f
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2013-10-16 08:33:36 +0200

    Add comments to sched

--- ordonnancement_collaboratif/process.h
+++ ordonnancement_collaboratif/process.h
@@ -1,23 +1,43 @@
 #include <stdint.h>
 
+//-------------------------
+//  Type definition
+//-------------------------
+
+// Used function by process
 typedef void (*func_t)(void);
 
+// Context structure definition
 typedef struct{
+	// Program counter
 	uint32_t pc;
+
+	// Stack Pointer
 	uint32_t sp;
+
+	//Registers
 	uint32_t registers [12];
 } ctx_s;
 
+// Context state definition
+typedef enum {created, ready, paused, active} ctx_state;
+
+// PCB structure to store process
 typedef struct{
 	//Context
 	ctx_s* ctx;
 	
-	//Actual state of the process : 0 = created, 1 = ready, 2 = paused, 3 = active
-	int state = 0;
+	//Actual state of the process
+	ctx_state state = created;
 
-	//Chained list mngt
+	//Chained list
 	pcb_s* next;	
 } pcb_s;
+
+
+//-------------------------
+//  Functions
+//-------------------------
 
 void init_pcb(func_t f, unsigned int stack_size);
 void init_ctx(ctx_s* ctx, func_t f, unsigned int stack_size);
--- ordonnancement_collaboratif/sched.c
+++ ordonnancement_collaboratif/sched.c
@@ -1,3 +1,4 @@
+// Global varible to store all PCBs
 pcb_s act_pcb;
 
 void yield() {
