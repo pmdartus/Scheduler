ce5908282719d8a9b59f008a8ddf6c7f24f3e397
Kim Chouard <kim.chouard@gmail.com>
2013-10-16 10:03:18 +0200

    [Bugfix] malloc -> AllocateMemory because all from scratch, no malloc definedgit add ordonnancement_collaboratif/process.c

--- ordonnancement_collaboratif/process.c
+++ ordonnancement_collaboratif/process.c
@@ -9,7 +9,7 @@
 //	Initialize pcb => from created to ready
 //-------------------------
 void init_pcb(pcb_s* pcb, func_t f, unsigned int stack_size) {	
-	ctx_s* new_ctx = malloc(sizeof(ctx_s));
+	ctx_s* new_ctx = AllocateMemory(sizeof(ctx_s));
 
 	pcb->ctx = new_ctx;
 	init_ctx( pcb->ctx, f, stack_size);
@@ -18,19 +18,20 @@ void init_pcb(pcb_s* pcb, func_t f, unsigned int stack_size) {
 	pcb->next = null;
 }
 
+
 //-------------------------
 //  init_ctx
 //	Initialize ctx, used in pcb
 //-------------------------
 void init_ctx(ctx_s* ctx, func_t f, unsigned int stack_size) {
 	//Initialisation of the context	
-	(*ctx).adr_instruction = (uint32_t) f;
+	(*ctx).pc = (uint32_t) f;
 	
 	//Stack allocation
 	uint32_t* new_loc = AllocateMemory(stack_size);
 	
 	//The stack adress is resturned by AllocateMemory. As the stack is decremented, we add the stack size to the stack start adress.
-	(*ctx).adr_stack = (uint32_t) new_loc + stack_size - 1; //because start at 0
+	(*ctx).sp = (uint32_t) new_loc + stack_size - 1; //because start at 0
 
 	int n;
 	for ( n=0 ; n<11 ; n++ )
