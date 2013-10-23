6118fbdcfbff1069361a6dbac2913c62daf66c6f
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2013-10-23 11:15:41 +0200

    Delete dispatcher module yet useless

--- ordonnancement_collaboratif/sched.c
+++ ordonnancement_collaboratif/sched.c
@@ -4,7 +4,6 @@
 
 #include "sched.h"
 #include "process.h"
-#include "dispatcher.h"
 #include "allocateMemory.h"
 
 // Global varible to store all PCBs
@@ -80,10 +79,29 @@ void schedule() {
 
 //-------------------------
 //  yield
-//	switch automatically the process, based on ctx_switch
+//	switch automatically the process
 //-------------------------
 void yield( ) {
-	ctx_switch();
+	//First, save all registers in stack
+	__asm("push {r0-r12, lr}");
+
+	//Save current context
+	__asm("mov %0, sp" : "=r"(act_pcb->sp)); //stack pointer
+	
+	schedule(); // this call changes act_pcb
+
+	//update the mc: assembleur to restaure new context
+	__asm("mov sp, %0" : : "r"(act_pcb->sp));
+
+	//If Proc never lunched
+	if (act_pcb->state == Ready) {
+		//Go to the entry point: the function
+		start_current_process();
+	}
+	else {
+		//pull all registers from stack
+		__asm("pop {r0-r12, lr}");
+	}
 }
 
 
